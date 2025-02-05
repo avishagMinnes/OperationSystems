0a2277ab48703b960595a9c1e7fa78a419a70109
avishagMinnes <157994250+avishagMinnes@users.noreply.github.com>
2025-02-05 14:20:44 +0200

    Update graph.h

--- graph.h
+++ graph.h
@@ -11,14 +11,10 @@ public:
     Graph(int n, int m);
     void addEdge(int u, int v);
     void removeEdge(int u, int v);
-    std::vector<std::vector<int>> kosaraju();
 
 private:
     int n, m;
     std::vector<std::list<int>> graph;
-    std::vector<std::list<int>> reverseGraph;
-    void dfs(int v, std::vector<bool>& visited, std::list<int>& Stack);
-    void reverseDfs(int v, std::vector<bool>& visited, std::vector<int>& component);
 };
 
 #endif // GRAPH_H
