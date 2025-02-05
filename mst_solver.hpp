7e693056d209c1fda66c19a29180acbad95de6e1
avishagMinnes <157994250+avishagMinnes@users.noreply.github.com>
2025-02-05 14:40:12 +0200

    Create mst_solver.hpp

---
+++ mst_solver.hpp
@@ -0,0 +1,14 @@
+#ifndef MST_SOLVER_HPP
+#define MST_SOLVER_HPP
+
+#include <vector>
+#include <list>
+
+class MSTSolver {
+public:
+    virtual ~MSTSolver() = default;
+    virtual int computeMST(int n, const std::vector<std::list<std::pair<int, int>>>& graph,
+                           std::vector<std::list<std::pair<int, int>>>& mstGraph) = 0;
+};
+
+#endif // MST_SOLVER_HPP
