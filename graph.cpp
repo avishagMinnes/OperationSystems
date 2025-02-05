4006e578c686573aa9b36cbd9460bd9e4b653afd
avishagMinnes <157994250+avishagMinnes@users.noreply.github.com>
2025-02-05 14:22:35 +0200

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -4,17 +4,14 @@
 
 Graph::Graph(int n, int m) : n(n), m(m) {
     graph.resize(n + 1);
-    reverseGraph.resize(n + 1);
 }
 
 void Graph::addEdge(int u, int v) {
     graph[u].push_back(v);
-    reverseGraph[v].push_back(u);
 }
 
 void Graph::removeEdge(int u, int v) {
     graph[u].remove(v);
-    reverseGraph[v].remove(u);
 }
 
 
